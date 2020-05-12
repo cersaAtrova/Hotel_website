9acb905dd012c96a6ca7af48132248ae40b46074
cersaatrova <soteris100@gmail.com>
2020-05-12 13:58:57 +0300

    insert footer menu

--- html/accomodation_rooms.php
+++ html/accomodation_rooms.php
@@ -26,8 +26,7 @@ require_once('functions.php')
     </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
-
-
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 
@@ -75,6 +74,8 @@ require_once('functions.php')
         }
         ?>
     </div>
+    <div style="height: 10vh"></div>
+    <?php footer() ?>
 </body>
 
 </html>
